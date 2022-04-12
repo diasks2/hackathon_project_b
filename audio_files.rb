070e77b1e05c83a762ba072a303ae4cf7abe71aa
Kevin Dias <diasks2@gmail.com>
2022-04-12 14:26:09 +0900

    Initial commit

---
+++ audio_files.rb
@@ -0,0 +1,404 @@
+[
+  {
+    files: ["0001.wav", "0001.ogg", "0001.mp3"],
+    word_phrase: "I want to eat egg",
+    sex: "male",
+    age_in_months: 25
+  },
+  {
+    files: ["0002.wav", "0002.ogg", "0002.mp3"],
+    word_phrase: "chimpanzee",
+    sex: "male",
+    age_in_months: 25
+  },
+  {
+    files: ["0003.wav", "0003.ogg", "0003.mp3"],
+    word_phrase: "gorilla",
+    sex: "male",
+    age_in_months: 25
+  },
+  {
+    files: ["0004.wav", "0004.ogg", "0004.mp3"],
+    word_phrase: "excavator",
+    sex: "male",
+    age_in_months: 25
+  },
+  {
+    files: ["0005.wav", "0005.ogg", "0005.mp3"],
+    word_phrase: "strawberry",
+    sex: "male",
+    age_in_months: 25
+  },
+  {
+    files: ["0006.wav", "0006.ogg", "0006.mp3"],
+    word_phrase: "all done",
+    sex: "male",
+    age_in_months: 25
+  },
+  {
+    files: ["0007.wav", "0007.ogg", "0007.mp3"],
+    word_phrase: "I ate everything",
+    sex: "male",
+    age_in_months: 62
+  },
+  {
+    files: ["0008.wav", "0008.ogg", "0008.mp3"],
+    word_phrase: "not yet",
+    sex: "male",
+    age_in_months: 25
+  },
+  {
+    files: ["0009.wav", "0009.ogg", "0009.mp3"],
+    word_phrase: "airplane",
+    sex: "male",
+    age_in_months: 25
+  },
+  {
+    files: ["0010.wav", "0010.ogg", "0010.mp3"],
+    word_phrase: "two",
+    sex: "male",
+    age_in_months: 62
+  },
+  {
+    files: ["0011.wav", "0011.ogg", "0011.mp3"],
+    word_phrase: "I see one Daddy",
+    sex: "male",
+    age_in_months: 25
+  },
+  {
+    files: ["0012.wav", "0012.ogg", "0012.mp3"],
+    word_phrase: "where's the bus?",
+    sex: "male",
+    age_in_months: 25
+  },
+  {
+    files: ["0013.wav", "0013.ogg", "0013.mp3"],
+    word_phrase: "mommy",
+    sex: "male",
+    age_in_months: 25
+  },
+  {
+    files: ["0014.wav", "0014.ogg", "0014.mp3"],
+    word_phrase: "daddy",
+    sex: "male",
+    age_in_months: 25
+  },
+  {
+    files: ["0015.wav", "0015.ogg", "0015.mp3"],
+    word_phrase: "monsters under my bed",
+    sex: "male",
+    age_in_months: 62
+  },
+  {
+    files: ["0016.wav", "0016.ogg", "0016.mp3"],
+    word_phrase: "strawberry",
+    sex: "male",
+    age_in_months: 62
+  },
+  {
+    files: ["0017.wav", "0017.ogg", "0017.mp3"],
+    word_phrase: "wacky Wednesday",
+    sex: "male",
+    age_in_months: 62
+  },
+  {
+    files: ["0018.wav", "0018.ogg", "0018.mp3"],
+    word_phrase: "taco Tuesday",
+    sex: "male",
+    age_in_months: 62
+  },
+  {
+    files: ["0019.wav", "0019.ogg", "0019.mp3"],
+    word_phrase: "ballet",
+    sex: "male",
+    age_in_months: 62
+  },
+  {
+    files: ["0020.wav", "0020.ogg", "0020.mp3"],
+    word_phrase: "funny face",
+    sex: "male",
+    age_in_months: 62
+  },
+  {
+    files: ["0021.wav", "0021.ogg", "0021.mp3"],
+    word_phrase: "chimpanzee",
+    sex: "male",
+    age_in_months: 62
+  },
+  {
+    files: ["0022.wav", "0022.ogg", "0022.mp3"],
+    word_phrase: "excavator",
+    sex: "male",
+    age_in_months: 62
+  },
+  {
+    files: ["0023.wav", "0023.ogg", "0023.mp3"],
+    word_phrase: "gorilla",
+    sex: "male",
+    age_in_months: 62
+  },
+  {
+    files: ["0024.wav", "0024.ogg", "0024.mp3"],
+    word_phrase: "all done",
+    sex: "male",
+    age_in_months: 62
+  },
+  {
+    files: ["0025.wav", "0025.ogg", "0025.mp3"],
+    word_phrase: "I ate everything",
+    sex: "male",
+    age_in_months: 62
+  },
+  {
+    files: ["0026.wav", "0026.ogg", "0026.mp3"],
+    word_phrase: "not yet",
+    sex: "male",
+    age_in_months: 62
+  },
+  {
+    files: ["0027.wav", "0027.ogg", "0027.mp3"],
+    word_phrase: "airplane",
+    sex: "male",
+    age_in_months: 62
+  },
+  {
+    files: ["0028.wav", "0028.ogg", "0028.mp3"],
+    word_phrase: "dog",
+    sex: "male",
+    age_in_months: 62
+  },
+  {
+    files: ["0029.wav", "0029.ogg", "0029.mp3"],
+    word_phrase: "cookie",
+    sex: "male",
+    age_in_months: 62
+  },
+  {
+    files: ["0030.wav", "0030.ogg", "0030.mp3"],
+    word_phrase: "rabbit",
+    sex: "male",
+    age_in_months: 62
+  },
+  {
+    files: ["0031.wav", "0031.ogg", "0031.mp3"],
+    word_phrase: "yellow",
+    sex: "male",
+    age_in_months: 62
+  },
+  {
+    files: ["0032.wav", "0032.ogg", "0032.mp3"],
+    word_phrase: "pan",
+    sex: "male",
+    age_in_months: 62
+  },
+  {
+    files: ["0033.wav", "0033.ogg", "0033.mp3"],
+    word_phrase: "fan",
+    sex: "male",
+    age_in_months: 62
+  },
+  {
+    files: ["0034.wav", "0034.ogg", "0034.mp3"],
+    word_phrase: "dump",
+    sex: "male",
+    age_in_months: 62
+  },
+  {
+    files: ["0035.wav", "0035.ogg", "0035.mp3"],
+    word_phrase: "jump",
+    sex: "male",
+    age_in_months: 62
+  },
+  {
+    files: ["0036.wav", "0036.ogg", "0036.mp3"],
+    word_phrase: "apple",
+    sex: "male",
+    age_in_months: 62
+  },
+  {
+    files: ["0037.wav", "0037.ogg", "0037.mp3"],
+    word_phrase: "paper",
+    sex: "male",
+    age_in_months: 62
+  },
+  {
+    files: ["0038.wav", "0038.ogg", "0038.mp3"],
+    word_phrase: "door",
+    sex: "male",
+    age_in_months: 62
+  },
+  {
+    files: ["0039.wav", "0039.ogg", "0039.mp3"],
+    word_phrase: "chips",
+    sex: "male",
+    age_in_months: 62
+  },
+  {
+    files: ["0040.wav", "0040.ogg", "0040.mp3"],
+    word_phrase: "shoe",
+    sex: "male",
+    age_in_months: 62
+  },
+  {
+    files: ["0041.wav", "0041.ogg", "0041.mp3"],
+    word_phrase: "fish",
+    sex: "male",
+    age_in_months: 62
+  },
+  {
+    files: ["0042.wav", "0042.ogg", "0042.mp3"],
+    word_phrase: "tie",
+    sex: "male",
+    age_in_months: 62
+  },
+  {
+    files: ["0043.wav", "0043.ogg", "0043.mp3"],
+    word_phrase: "pie",
+    sex: "male",
+    age_in_months: 62
+  },
+  {
+    files: ["0044.wav", "0044.ogg", "0044.mp3"],
+    word_phrase: "bus",
+    sex: "male",
+    age_in_months: 62
+  },
+  {
+    files: ["0045.wav", "0045.ogg", "0045.mp3"],
+    word_phrase: "nose",
+    sex: "male",
+    age_in_months: 62
+  },
+  {
+    files: ["0046.wav", "0046.ogg", "0046.mp3"],
+    word_phrase: "doze",
+    sex: "male",
+    age_in_months: 62
+  },
+  {
+    files: ["0047.wav", "0047.ogg", "0047.mp3"],
+    word_phrase: "pig",
+    sex: "male",
+    age_in_months: 62
+  },
+  {
+    files: ["0048.wav", "0048.ogg", "0048.mp3"],
+    word_phrase: "pick",
+    sex: "male",
+    age_in_months: 62
+  },
+  {
+    files: ["0049.wav", "0049.ogg", "0049.mp3"],
+    word_phrase: "comb",
+    sex: "male",
+    age_in_months: 62
+  },
+  {
+    files: ["0050.wav", "0050.ogg", "0050.mp3"],
+    word_phrase: "spoon",
+    sex: "male",
+    age_in_months: 62
+  },
+  {
+    files: ["0051.wav", "0051.ogg", "0051.mp3"],
+    word_phrase: "bottle",
+    sex: "male",
+    age_in_months: 62
+  },
+  {
+    files: ["0052.wav", "0052.ogg", "0052.mp3"],
+    word_phrase: "pane",
+    sex: "male",
+    age_in_months: 62
+  },
+  {
+    files: ["0053.wav", "0053.ogg", "0053.mp3"],
+    word_phrase: "plane",
+    sex: "male",
+    age_in_months: 62
+  },
+  {
+    files: ["0054.wav", "0054.ogg", "0054.mp3"],
+    word_phrase: "toe",
+    sex: "male",
+    age_in_months: 62
+  },
+  {
+    files: ["0055.wav", "0055.ogg", "0055.mp3"],
+    word_phrase: "toad",
+    sex: "male",
+    age_in_months: 62
+  },
+  {
+    files: ["0056.wav", "0056.ogg", "0056.mp3"],
+    word_phrase: "bunny",
+    sex: "male",
+    age_in_months: 62
+  },
+  {
+    files: ["0057.wav", "0057.ogg", "0057.mp3"],
+    word_phrase: "funny",
+    sex: "male",
+    age_in_months: 62
+  },
+  {
+    files: ["0058.wav", "0058.ogg", "0058.mp3"],
+    word_phrase: "banana",
+    sex: "male",
+    age_in_months: 62
+  },
+  {
+    files: ["0059.wav", "0059.ogg", "0059.mp3"],
+    word_phrase: "blue",
+    sex: "male",
+    age_in_months: 62
+  },
+  {
+    files: ["0060.wav", "0060.ogg", "0060.mp3"],
+    word_phrase: "fire engine",
+    sex: "male",
+    age_in_months: 25
+  },
+  {
+    files: ["0061.wav", "0061.ogg", "0061.mp3"],
+    word_phrase: "ambulance",
+    sex: "male",
+    age_in_months: 25
+  },
+  {
+    files: ["0062.wav", "0062.ogg", "0062.mp3"],
+    word_phrase: "milk",
+    sex: "male",
+    age_in_months: 25
+  },
+  {
+    files: ["0063.wav", "0063.ogg", "0063.mp3"],
+    word_phrase: "fork",
+    sex: "male",
+    age_in_months: 25
+  },
+  {
+    files: ["0064.wav", "0064.ogg", "0064.mp3"],
+    word_phrase: "spoon",
+    sex: "male",
+    age_in_months: 25
+  },
+  {
+    files: ["0065.wav", "0065.ogg", "0065.mp3"],
+    word_phrase: "monster",
+    sex: "male",
+    age_in_months: 25
+  },
+  {
+    files: ["0066.wav", "0066.ogg", "0066.mp3"],
+    word_phrase: "funny face",
+    sex: "male",
+    age_in_months: 25
+  },
+  {
+    files: ["0067.wav", "0067.ogg", "0067.mp3"],
+    word_phrase: "bat hat",
+    sex: "male",
+    age_in_months: 25
+  },
+]
